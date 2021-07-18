902e8a7ea9ab0e01e768254650aa537fca720188
Subramanian <subramanian_karuppiah@cpf.gov.sg>
2021-07-18 13:00:53 +0800

    addedd

--- test/SimpleAPI.Test/UnitTest1.cs
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI.Test
         public void GetReturnsMyname()
         {
             IEnumerable returnValue = controller.Get();
-            Assert.Equal("Subbu1","Subbu1");
+            Assert.Equal("Subbu21","Subbu21");
         }
 
 
