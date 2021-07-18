4886ec9823d11f051bd1608b69c15792d50cd4ed
Subramanian <subramanian_karuppiah@cpf.gov.sg>
2021-07-18 11:48:24 +0800

    push

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+         public static ILogger<WeatherForecastController> _logger;
+        WeatherForecastController controller = new WeatherForecastController(_logger);
+        [Fact]
+        public void GetReturnsMyname()
+        {
+            IEnumerable returnValue = controller.Get();
+            Assert.Equal("Subbu","Subbu");
+        }
+
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
